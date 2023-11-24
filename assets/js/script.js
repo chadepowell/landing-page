f40871223901cf7cc4196fc6596ccf703173172a
Chade Powell <lashaepowell98@gmail.com>
2023-11-24 11:32:44 -0600

    remove nav styles

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,23 @@
 'use strict';
+let navBtn = document.querySelector('nav .btn');
+
+if (navBtn) {
+    navBtn.addEventListener( 'click', e => {
+        e.preventDefault();
+
+        let navMenu = document.querySelector('nav ul');
+
+        navMenu.classList.btn('show');
+        /*show x*/
+        if (navMenu.classList.contains('show')) {
+            navBtn.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x" viewBox="0 0 16 16">
+            <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
+          </svg>`
+          /*Do not show x */
+        } else {
+            navBtn.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">
+            <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"></path>
+        </svg>`
+        }
+    } )
+}
