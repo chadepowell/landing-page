6941021267bcdbc6b031f1679b3f5c83e21766da
Chade Powell <lashaepowell98@gmail.com>
2023-11-25 14:04:55 -0600

    adjust styles for section-2

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,8 @@
 'use strict';
-let navToggler = document.querySelector('nav .btn');
+let navToggler = document.querySelector('nav .toggler');
 
+console.log(navToggler)
+console.log("navToggler")
 if (navToggler) {
     navToggler.addEventListener( 'click', e => {
         e.preventDefault();
@@ -10,7 +12,7 @@ if (navToggler) {
         navMenu.classList.toggle('show');
         /*show x*/
         if (navMenu.classList.contains('show')) {
-            navBtn.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x" viewBox="0 0 16 16">
+            navToggler.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x" viewBox="0 0 16 16">
             <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
           </svg>`
           /*Do not show x */
