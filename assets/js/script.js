297d51374ca6cac40f42f7951bd1592c97be9547
Chade Powell <lashaepowell98@gmail.com>
2023-11-25 09:47:50 -0600

    hero section styles fixes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,13 @@
 'use strict';
-let navBtn = document.querySelector('nav .btn');
+let navToggler = document.querySelector('nav .btn');
 
-if (navBtn) {
-    navBtn.addEventListener( 'click', e => {
+if (navToggler) {
+    navToggler.addEventListener( 'click', e => {
         e.preventDefault();
 
         let navMenu = document.querySelector('nav ul');
 
-        navMenu.classList.btn('show');
+        navMenu.classList.toggle('show');
         /*show x*/
         if (navMenu.classList.contains('show')) {
             navBtn.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x" viewBox="0 0 16 16">
@@ -15,7 +15,7 @@ if (navBtn) {
           </svg>`
           /*Do not show x */
         } else {
-            navBtn.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">
+            navToggler.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">
             <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"></path>
         </svg>`
         }
